7fe073b2f29874afa21634e2d7f169b4ad4abaa4
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2024-04-02 14:07:33 -0300

    Update tabelas_faculdade.sql

--- tabelas_faculdade.sql
+++ tabelas_faculdade.sql
@@ -88,22 +88,19 @@ create table alunos_materias(
     primary key (alunoID, materiaID)
 );
 
-insert into alunos_materias values(1,1);
-insert into alunos_materias values(2,1);
-insert into alunos_materias values(3,2);
-insert into alunos_materias values(1,3);
-insert into alunos_materias values(1,4);
-insert into alunos_materias values(3,4);
-insert into alunos_materias values(1,5);
-insert into alunos_materias values(10,6);
-insert into alunos_materias values(5,10);
-insert into alunos_materias values(6,10);
-insert into alunos_materias values(1,7);
-insert into alunos_materias values(2,8);
-insert into alunos_materias values(7,8);
-insert into alunos_materias values(9,9);
-insert into alunos_materias values(8,2);
-insert into alunos_materias values(10,3);
+insert into alunos_materias 
+values
+	(1,1), (2,1), (3,1), (4,1), (5,1),
+    (1,2),        (3,2),        (5,2),        (7,2), (8,2), (9,2),
+    (1,3), (2,3), (3,3), (4,3),        (6,3),        (8,3), (9,3), (10,3),
+    (1,4), (2,4),
+           (2,5), (3,5), (4,5), (5,5),
+                                       (6,6), (7,6), (8,6), (9,6), (10,6),
+                                              (7,7),
+                                       (6,8),        (8,8), (9,8), (10,8),
+    (1,9), (2,9),
+    (1,10),(2,10),(3,10),(4,10),(5,10),(6,10),(7,10),(8,10),(9,10),(10,10);
+show tables;
 
 create table alunos_professores(
     alunoID int references aluno(id_aluno),
@@ -111,19 +108,16 @@ create table alunos_professores(
     primary key (alunoID, professorID)
 );
 
-insert into alunos_professores values(1,1);
-insert into alunos_professores values(2,2);
-insert into alunos_professores values(3,2);
-insert into alunos_professores values(4,5);
-insert into alunos_professores values(5,7);
-insert into alunos_professores values(6,3);
-insert into alunos_professores values(7,4);
-insert into alunos_professores values(8,4);
-insert into alunos_professores values(9,1);
-insert into alunos_professores values(1,7);
-insert into alunos_professores values(3,1);
-insert into alunos_professores values(10,5);
-insert into alunos_professores values(9,6);
+insert into alunos_professores 
+values
+	(1,1), (2,1),        (4,1), (5,1), (6,1), (7,1), (8,1), (9,1), (10,1),
+    (1,2), (2,2), (3,2), (4,2), (5,2),
+    (1,3), (2,3),                      (6,3), (7,3),        (9,3), (10,3),
+    (1,4),        (3,4),        (5,4), (6,4),
+                                (5,5),                             (10,5),
+                                       (6,6), (7,6), (8,6), (9,6), (10,6),
+           (2,7), (3,7), (4,7),        (6,7), (7,7), (8,7),        (10,7);
+show tables;
 
 create table materias_professores(
     materiaID int references materia(id_materia),
@@ -131,19 +125,82 @@ create table materias_professores(
     primary key (materiaID, professorID)
 );
 
-insert into materias_professores values(1,1);
-insert into materias_professores values(2,1);
-insert into materias_professores values(3,7);
-insert into materias_professores values(3,5);
-insert into materias_professores values(4,2);
-insert into materias_professores values(5,2);
-insert into materias_professores values(6,3);
-insert into materias_professores values(7,6);
-insert into materias_professores values(8,5);
-insert into materias_professores values(9,4);
-insert into materias_professores values(10,7);
-insert into materias_professores values(10,2);
+insert into materias_professores 
+values
+	(1,1), (2,1),
+    (3,2),
+    (4,3),
+    (5,4), (6,4),
+    (8,5),
+    (9,6),
+    (10,7);
+show tables;
+
+
+/*
+Codigo que encontra os professores de um aluno específico
+*/
+SELECT professor.nome_professor
+FROM aluno
+JOIN alunos_professores ON aluno.id_aluno = alunos_professores.alunoID
+JOIN professor ON alunos_professores.professorID = professor.id_professor
+WHERE aluno.id_aluno = 2;
+
+/*
+Codigo que diz quais matérias tem mais de 3 alunos matriculados
+*/
 
 
 
+
+
+
+
+/*
+Codigo que diz quais são os alunos de uma determinada matéria 
+*/
+SELECT a.nome
+FROM aluno a
+JOIN alunos_materias am ON a.id_aluno = am.alunoID
+JOIN materia m ON am.materiaID = m.id_materia
+WHERE m.nome_materia = "Fisica";
+
+/*
+código que conta quantos alunos tem em cada matéria
+*/
+SELECT m.nome_materia, COUNT(am.alunoID) AS numero_alunos
+FROM materia m
+INNER JOIN alunos_materias am ON m.id_materia = am.materiaID
+GROUP BY m.nome_materia;
+
+/*
+código que verifica se existe alguma matéria sem professor atribuído
+*/
+SELECT materia.nome_materia
+FROM materia
+WHERE id_materia NOT IN (
+    SELECT DISTINCT materiaID
+    FROM materias_professores
+);
+
+/*
+código que lista TODOS os alunos de um determinado professor. É possível evitar repetições de nomes
+*/
+SELECT aluno.nome
+FROM professor
+JOIN alunos_professores ON professor.id_professor = alunos_professores.professorID
+JOIN aluno ON alunos_professores.alunoID = aluno.id_aluno
+WHERE professor.id_professor = 1;
+
+/*
+código que conta a quantidade de professor
+*/
+SELECT COUNT(*) AS total_professores
+FROM professor;
+
+/*
+código que conta quantos alunos estao matriculados
+*/
+SELECT COUNT(*) AS total_aluno
+FROM aluno;
 
